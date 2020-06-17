44a02cf55fe2c509d1952f48f67dda253b2b9e71
Gautier VUILLAUME <gautier.vuillaume@viacesi.fr>
2020-06-17 22:42:03 +0200

    comments

--- src/utils/ArgParser.java
+++ src/utils/ArgParser.java
@@ -3,27 +3,27 @@ package utils;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ArgParser {//TODO : comm
+public class ArgParser {//class used to get arguments from the command execution
 	
 	private static String[] argList = {"-results", "-repo", "-truth"}; 
 	
 	public static Map<String, String> parseArg(String[] args){
 		
 		Map<String, String> parsedArgs = new HashMap<String, String>();
+		//Checks each value of the args from the execution
 		for(int i=0; i<args.length;i++) {
-				if(isArg(args[i])) {parsedArgs.put(args[i], args[i+1]);
-			}
+			//checks if the -name is existing, if so add it 
+			if(isArg(args[i])) {parsedArgs.put(args[i], args[i+1]);}
 		}
 		
 		return parsedArgs;
 	}
 	
+	//checking array of names if the parameters is listed
 	private static boolean isArg(String arg) {
-		
 		for(String argElem : argList) {
 			if(arg.equals(argElem)) {return true;}
 		}
-		
 		return false;
 	}
 }
--- src/utils/Bug.java
+++ src/utils/Bug.java
@@ -10,21 +10,18 @@ import java.util.List;
 
 import utilmport.getJIRAInfo;
 
-public class Bug { //TODO : comm
-	public List<CommitSZZ> commitIntroducersSZZ;
-	public Commit commitIntroducerReal;
-	public Commit commitFix;
+public class Bug {
+	public List<CommitSZZ> commitIntroducersSZZ; //Potential introducers flagged by SZZ
+	public Commit commitIntroducerReal; //Real introducer from the database
+	public Commit commitFix;	//Fixing commit
 	
 	public Bug(String hashFix, List<CommitSZZ> commitIntroducersSZZ) {
 		this.commitFix = new Commit(hashFix);
-		this.commitIntroducersSZZ = new LinkedList<CommitSZZ>();
-		
 		this.commitIntroducersSZZ = commitIntroducersSZZ;
-		
 		this.commitIntroducerReal = new Commit(getJIRAInfo.getRealIntroducer(hashFix));
-		
 	}
 	
+	//Compute earliest bug appearance, checking if potential commits are younger than introducer commit
 	public boolean earliestBugAppearance() {
 		for(CommitSZZ introducer : commitIntroducersSZZ) {
 			if(introducer.date.after(commitIntroducerReal.date)) {
@@ -34,14 +31,15 @@ public class Bug { //TODO : comm
 		return false;
 	}
 	
+	//Compute single bug precision
 	public boolean hasWellPredicted() {
-		
 		for(CommitSZZ introducer : commitIntroducersSZZ) {
 			if(introducer.hashCode.equals(commitIntroducerReal.hashCode)) {return true;}
 		}
 		return false;
 	}
 	
+	//Compute time span between the oldest and the youngest potential introducer of the bug
 	public Integer potentialIntroducerTimeSpan() {
 		
 		List<Date> dates = new LinkedList<Date>();
@@ -56,7 +54,6 @@ public class Bug { //TODO : comm
 	    LocalDateTime date1 = dates.get(0).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
 	    Integer daysBetween = (int) Duration.between(date1, date2).toDays();
 	    
-		
 		return daysBetween;
 	}
 	
